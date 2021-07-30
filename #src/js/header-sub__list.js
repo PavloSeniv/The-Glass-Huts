2b6ef59f3ba05d1b394fc1ff96f5d8bedd87dda4
Seniv Pavlo <senivpavlo016@gmail.com>
2021-07-30 20:20:27 +0300

    Add header menu and add folder facebiluty

--- #src/js/header-sub__list.js
+++ #src/js/header-sub__list.js
@@ -1,24 +1,3 @@
-//Початок коду для додавання webp формату у css
-// JS-функція визначення підтримки WebP
-function testWebP(callback) {
-
-    var webP = new Image();
-    webP.onload = webP.onerror = function () {
-        callback(webP.height === 2);
-    };
-    webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
-}
-
-testWebP(function (support) {
-
-    if (support === true) {
-        document.querySelector('body').classList.add('webp');
-    } else {
-        document.querySelector('body').classList.add('no-webp');
-    }
-});
-//Кінець
-
 /*Перевірка на тип пристрою*/
 "use strict"
 const isMobile = {
--- #src/js/script.js
+++ #src/js/script.js
@@ -1,3 +1,5 @@
 //Початок коду для додавання webp формату у css
 @@include('WebpAddClassToHtml.js')
 //Кінець
+
+@@include('header-sub__list.js')
